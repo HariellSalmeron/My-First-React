d7dee4f8373467c7909d2d84334b177a6fcda7b1
Hariell Salmeron <hariellsalmeron@gmail.com>
2024-07-12 17:01:04 +0800

    first-react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hariell Salmeron G</h1>
+      <h2>Baby John Acidillo</h2>
+    </div>
+  );
+}
+
+export default App;
